9b3092de3287f9b158e2e310a4621d56840100a5
xavier <xavier.kress@gmail.com>
2019-11-17 17:37:14 +0100

    make injection works by extending a Spring Data Repository interface

--- src/main/java/com/xavierkress/springdata/basicExample/repositories/BookRepository.java
+++ src/main/java/com/xavierkress/springdata/basicExample/repositories/BookRepository.java
@@ -1,7 +1,9 @@
 package com.xavierkress.springdata.basicExample.repositories;
 
 
+import com.xavierkress.springdata.basicExample.model.Book;
+import org.springframework.data.repository.Repository;
 
-public interface BookRepository {
+public interface BookRepository extends Repository<Book, Long> {
 
 }
